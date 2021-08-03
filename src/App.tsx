476940039589a102b4cf5f3f98b05e76719e59df
gopal-velotio <gopal.zalke@velotio.com>
2021-08-03 16:48:52 +0530

    fetch users and show grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import Users from 'features/users/Users';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserDetails from 'features/userDetails/UserDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Users />
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path="/" exact component={Users} />
+          <Route path="/users" exact component={Users} />
+          <Route path="/user/:id" exact component={UserDetails} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
